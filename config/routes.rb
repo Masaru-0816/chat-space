47d589ab53070e6d0ec51edb10e949bf3a4ccee8
Masaru-0816 <two-of-us.0816@hotmail.co.jp>
2020-01-10 21:07:19 +0900

    2nd commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "messages#index"
-
-  resources :users, only: [:edit, :update, :destroy]
-  resources :groups, only: [:new, :create, :edit, :update]
+  root "groups#index"
+  resources :users, only: [:edit, :update]
+  resources :groups, only: [:index, :new, :create, :edit, :update]
 end
